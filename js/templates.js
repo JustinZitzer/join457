42ea245bced483fe8ea3aacb1aa51ccc8e6fe4af
JustinZitzer <justinzi@web.de>
2025-04-25 19:40:20 +0200

    added template for navLink

---
+++ js/templates.js
@@ -0,0 +1,30 @@
+function getNavLinksTemplate() {
+    return `
+        <ul class="nav-links">
+            <li>
+                <a>
+                    <img src="./assets/icons/summary-icon.svg" alt="summary-icon">
+                    <span>Summary</span>
+                </a>
+            </li>
+            <li>
+                <a>
+                    <img src="./assets/icons/add-task-icon.svg" alt="summary-icon">
+                    <span>Add Task</span>
+                </a>
+            </li>
+            <li>
+                <a>
+                    <img src="./assets/icons/board-icon.svg" alt="summary-icon">
+                    <span>Board</span>
+                </a>
+            </li>
+            <li>
+                <a>
+                    <img src="./assets/icons/contacts-icon.svg" alt="summary-icon">
+                    <span>Contacts</span>
+                </a>
+            </li>
+        </ul>
+    `;
+}
