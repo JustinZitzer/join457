a7aca5b0e1299088ed5f7b65fd81b9709ebe8d37
JustinZitzer <justinzi@web.de>
2025-04-30 18:48:59 +0200

    added style to contacts html

---
+++ join/js/templates.js
@@ -0,0 +1,12 @@
+function getNavLinksTemplate() {
+    return `
+        <ul class="nav-links">
+            <li>
+                <a>
+                    <img src="./assets/icons/log-in-icon.svg" alt="log-in-icon">
+                    <span>Log In</span>
+                </a>
+            </li>
+        </ul>
+    `;
+}
