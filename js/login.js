02efd8f793a41be9266b08a344de4ee2e928337d
Marvin Mutwil <marvinmutwil@hotmail.de>
2025-05-07 23:45:01 +0200

    Guest Login function is ready.
    The Guest Login is now correct you can iterate trough the menu on the left and there will be no name in the greeting div. I save the guestName in sthe session storage. If Name is there, we add display-none to the name greeting.

--- js/login.js
+++ js/login.js
@@ -1,4 +1,18 @@
+function initSummary () {
+    getLoginGuest();
+}
+
 function loginGuest () {
+    const guestName = 'Gast';
+    sessionStorage.setItem('username', guestName);
     window.location.href='summary-guest.html'
-    
+}
+
+function getLoginGuest () {
+    const guestSummary = sessionStorage.getItem('username');
+    const summaryNameTextDiv = document.getElementById('Summary-Name-Text-Greeting');
+
+    if (guestSummary) {
+        summaryNameTextDiv.classList.add('display-none');
+    }
 }
