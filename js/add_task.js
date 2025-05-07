b49d7121e5ee46ec152bd893366a7109db42ae0a
ErhanGuelsoy <139971267+ErhanGuelsoy@users.noreply.github.com>
2025-05-07 18:13:09 +0200

    update board

---
+++ js/add_task.js
@@ -0,0 +1,13 @@
+
+
+function openOverlayAddTask() {
+    let openOverlayAddTask = document.getElementById('content-add-task-overlay');
+    openOverlayAddTask.classList.add('d-none');
+
+}
+
+function closeOverlayAddTask() {
+    let closeOverlayAddTask = document.getElementById('content-add-task-overlay');
+    closeOverlayAddTask.classList.remove('d-none')
+
+}
