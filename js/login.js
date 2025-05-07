d28e4f4d98bdc90cee9c5028cb0c1215e09a4668
Marvin Mutwil <marvinmutwil@hotmail.de>
2025-05-08 00:31:49 +0200

    login function progression

--- js/login.js
+++ js/login.js
@@ -1,22 +1,45 @@
+const emailInput = document.getElementById('Login-Box-Email-Input');
+const passwordInput = document.getElementById('Login-Box-Passwort-Input');
+
 function initSummary () {
     getLoginGuest();
+    getLogin();
 }
 
 function login() {
-    
+    const email = emailInput.value;
+    const password = passwordInput.value;
+    sessionStorage.setItem('username', email);
+    if (email && password) {
+        window.location.href='summary.html';
+    } else {
+        alert('Du dummer Hund gib jetzt ein anständiges Passwort und eine gültige Email ein');
+    }
+}
+
+function getLogin () {
+    let summaryNameTextDiv = document.getElementById('Summary-Name-Text-Greeting');
+    const username = sessionStorage.getItem('username');
+    const name = username.split('@')[0];
+    const dotIndex = localPart.indexOf('.');
+    const first = localPart.substring(0, dotIndex);
+    const last  = localPart.substring(dotIndex + 1);
+    if (username) {
+        summaryNameTextDiv.innerHTML = `${name}`;
+    }
 }
 
 function loginGuest () {
     const guestName = 'Gast';
-    sessionStorage.setItem('username', guestName);
+    sessionStorage.setItem('guestUsername', guestName);
     window.location.href='summary-guest.html'
 }
 
 function getLoginGuest () {
-    const guestSummary = sessionStorage.getItem('username');
+    const usernameGuest = sessionStorage.getItem('guestUsername');
     const summaryNameTextDiv = document.getElementById('Summary-Name-Text-Greeting');
 
-    if (guestSummary) {
+    if (usernameGuest) {
         summaryNameTextDiv.classList.add('display-none');
     }
 }
