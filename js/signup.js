d959c6562e9646ee091efbc4916ad8b28d820793
Marvin Mutwil <marvinmutwil@hotmail.de>
2025-05-09 23:48:42 +0200

    const for inputs in signup.js  added

--- js/signup.js
+++ js/signup.js
@@ -1,4 +1,9 @@
 const FireBaseUrl = "https://join-457-default-rtdb.europe-west1.firebasedatabase.app/";
+const nameInputSignUp = document.getElementById('inputfield-name');
+const emailInputSignUp = document.getElementById('inputfield-email');
+const passwordInputSignUp = document.getElementById('inputfield-password');
+const confirmPasswordInputSignUp = document.getElementById('inputfield-confirm');
+
 
 async function init() {
   await loadData();
