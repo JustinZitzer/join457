73df7ebc7916bd94530b894ecf7f128f3bc7f08a
Marvin Mutwil <marvinmutwil@hotmail.de>
2025-05-11 04:18:33 +0200

    Login function finished

--- js/summary.js
+++ js/summary.js
@@ -1,9 +1,19 @@
 const summaryGreetingTextName = document.getElementById("Summary-Name-Text-Greeting");
 const fireBaseUrlSummary = "https://join-457-default-rtdb.europe-west1.firebasedatabase.app/";
 
-async function getInfoForSummaryBoard (path="") {
-    let response = await fetch(FireBaseUrl + path + ".json");
-    let responseToJson = await response.json();
-    return responseToJson;
+async function initSummaryBoard() {
+    await getInformationSummaryBoard("/userData");
+}
+
+async function getInfoForSummaryBoardBaseFunction (path) {
+    let response = await fetch(fireBaseUrlSummary + path + ".json");
+    return await response.json();
+
+}
+
+async function getInformationSummaryBoard(path) {
+    let userDataSummary = await getInfoForSummaryBoardBaseFunction(path);
+    const firstUser = Object.values(userDataSummary)[0];
+    summaryGreetingTextName.innerHTML = firstUser.name;
 }
 
