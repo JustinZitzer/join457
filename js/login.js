dce0561e079c60397ef0679997492af590a4d8e0
Marvin Mutwil <marvinmutwil@hotmail.de>
2025-05-12 03:02:09 +0200

    Login Animation finished

--- js/login.js
+++ js/login.js
@@ -1,6 +1,7 @@
 const emailInput = document.getElementById('Login-Box-Email-Input');
 const passwordInput = document.getElementById('Login-Box-Passwort-Input');
 const FireBaseUrl = "https://console.firebase.google.com/u/1/project/join-457/database/join-457-default-rtdb/data/~2F";
+const loginBodyDiv = document.getElementById('Login-Body-Div');
 
 function initSummary () {
     getLoginGuest();
@@ -11,7 +12,7 @@ window.onload = function () {
     const logo = document.getElementById('loading-screen-logo');
     setTimeout(() => {
       logo.classList.add('loading-screen-logo-small');
-    }, 100); // Sofort beim Laden
+    }, 10); // Sofort beim Laden
 };
 
 function login() {
