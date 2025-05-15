defbe84456f3e9d9c08855921d2c5a18f63ce6af
ErhanGuelsoy <139971267+ErhanGuelsoy@users.noreply.github.com>
2025-05-15 12:20:17 +0200

    update overlays board

--- js/add_task.js
+++ js/add_task.js
@@ -121,123 +121,120 @@ function closeOverlay(event) {
 
 function overlayToDo() {
     const overlayToDoContainer = document.getElementById('overlay-todo');
-    overlayToDoContainer.classList.add('overlay-visible')
-    let overlayContentToDo = document.getElementById('content-add-task-overlay-todo');
+    overlayToDoContainer.classList.add('overlay-visible');
+
+    const overlayContentToDo = document.getElementById('content-add-task-overlay-todo');
+
+    // Inhalt leeren, um Mehrfach-Hinzufügen zu verhindern
+    overlayContentToDo.innerHTML = '';
+
     overlayContentToDo.innerHTML += `
-    <div class="container-both-sides">
-            <div class="inputfield-box-left-side">
-                <div class="text-title">
-                    <div class="text-title-mark">
-                        <span>Title</span>
-                    </div>
-                    <input type="text" placeholder="Enter a title" class="title-inputfield-enter-title">
-                </div>
-                <div class="description-main-box">
-                    <div class="description-text">
-                        <span>Description</span>
-                        <input type="text" placeholder="Enter Description" class="inputfield-description">
-                    </div>
-                    <div class="inputfield-due-date-container">
-                        <img class="calendar-clock" src="./assets/icons/calendar_clock.png" alt="clock-calendar">
-
-                        <div class="due-date-mark">
-                            <span>Due date</span>
+        <div class="overlay-todo-contentTodo" onclick="event.stopPropagation()">
+            <div class="container-both-sides">
+                <div class="inputfield-box-left-side">
+                    <div class="text-title">
+                        <div class="text-title-mark">
+                            <span>Title</span>
                         </div>
-                        <input type="text" placeholder="dd/mm/yyy" class="due-date-text-field">
+                        <input type="text" placeholder="Enter a title" class="title-inputfield-enter-title">
+                    </div>
+                    <div class="description-main-box">
+                        <div class="description-text">
+                            <span>Description</span>
+                            <input type="text" placeholder="Enter Description" class="inputfield-description">
+                        </div>
+                        <div class="inputfield-due-date-container">
+                            <img class="calendar-clock" src="./assets/icons/calendar_clock.png" alt="clock-calendar">
+                            <div class="due-date-mark">
+                                <span>Due date</span>
+                            </div>
+                            <input type="text" placeholder="dd/mm/yyyy" class="due-date-text-field">
+                        </div>
+                    </div>
+                </div>
+                <img class="line-middle-of-both-container" src="./assets/icons/Vector 4.png" alt="line between bothcontainers">
+                <div class="inputfield-box-right-side">
+                    <div class="text-priority">
+                        <div class="priority-box">
+                            <span>Priority</span>
+                            <div class="main-box-inputfield">
+                                <div class="arrow-container-main-red">
+                                    <button class="arrow-container-red">
+                                        <h4>Urgent</h4>
+                                        <img class="arrow-red" src="./assets/icons/double-arrow-up-14221.png" alt="red-arrow">
+                                    </button>
+                                </div>
+                                <input type="text" placeholder="Medium =" class="text-inputfield-medium">
+                                <div class="arrow-container-main-green">
+                                    <button class="arrow-container-green">
+                                        <h5>Low</h5>
+                                        <img class="arrow-green" src="./assets/icons/double-arrow-down-14228.png" alt="green-arrow">
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="assigned-inputfield-box">
+                            <span>Assigned to</span>
+                            <input type="text" placeholder="Select contacts to assign" class="inputfield-text-assign">
+                            <img class="assigned-arrow-icon" src="./assets/icons/arrow_drop_down.png" alt="assigned-arrow-button">
+                        </div>
+                        <div class="task-inputfield-box">
+                            <div class="category-mark">
+                                <span>Category</span>
+                            </div>
+                            <input type="text" placeholder="Select task category" class="inputfield-category-assign">
+                            <img class="assigned-arrow-icon-down" src="./assets/icons/arrow_drop_down.png" alt="assigned-arrow-button">
+                        </div>
+                        <div class="subtask-inputfield-box">
+                            <span>Subtasks</span>
+                            <div class="subtask-inputfield-container">
+                                <img class="add-icon-container" src="./assets/icons/subtask-plus-icon.svg" alt="subtask-plus-icon">
+                                <input type="text" placeholder="Add new subtask" class="inputfield-subtask-assign">
+                            </div>
+                        </div>
+                    </div>
+                    <div class="field-required">
+                        <span>This field is required</span>
                     </div>
                 </div>
             </div>
-            <img class="line-middle-of-both-container" src="./assets/icons/Vector 4.png"
-                alt="line between bothcontainers">
-
-            <div class="inputfield-box-right-side">
-                <div class="text-priority">
-                    <div class="priority-box">
-                        <span>Priority</span>
-
-                        <div class="main-box-inputfield">
-                            <div class="arrow-container-main-red">
-                                <button class="arrow-container-red">
-                                    <h4>Urgent</h4>
-                                    <img class="arrow-red" src="./assets/icons/double-arrow-up-14221.png"
-                                        alt="red-arrow">
-                                </button>
-                            </div>
-
-                            <input type="text" placeholder="Medium =" class="text-inputfield-medium">
-
-                            <div class="arrow-container-main-green">
-                                <button class="arrow-container-green">
-                                    <h5>Low</h5>
-                                    <img class="arrow-green" src="./assets/icons/double-arrow-down-14228.png"
-                                        alt="green-arrow">
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="assigned-inputfield-box">
-                        <span>Assigned to</span>
-                        <input type="text" placeholder="Select contacts to assign" class="inputfield-text-assign">
-
-                        <img class="assigned-arrow-icon" src="./assets/icons/arrow_drop_down.png"
-                            alt="assigned-arrow-button">
-
-                    </div>
-                    <div class="task-inputfield-box">
-                        <div class="category-mark">
-                            <span>Category</span>
-                        </div>
-                        <input type="text" placeholder="Select task category" class="inputfield-category-assign">
-                        <img class="assigned-arrow-icon-down" src="./assets/icons/arrow_drop_down.png"
-                            alt="assigned-arrow-button">
-                    </div>
-
-                    <div class="subtask-inputfield-box">
-                        <span>Subtasks</span>
-                        <div class="subtask-inputfield-container">
-                            <img class="add-icon-container" src="./assets/icons/subtask-plus-icon.svg"
-                                alt="subtask-plus-icon">
-                            <input type="text" placeholder="Add new subtask" class="inputfield-subtask-assign">
-                        </div>
-                    </div>
-                    
+            <div class="clear-create-container">
+                <div class="clear-field">
+                    <div class="clear-text">Clear</div>
+                    <img class="close-button" src="./assets/icons/close.png" alt="close-icon">
                 </div>
-                <div class="field-required">
-                    <span>This field is required</span>
-                    
+                <div class="create-field">
+                    <div class="text-create-field">Create Task</div>
+                    <img class="check-button" src="./assets/icons/check.png" alt="check-button">
                 </div>
             </div>
-            
         </div>
-        
-    <div class="clear-create-container">
-        <div class="clear-field">
-            <div class="clear-text">Clear</div>
-            <img class="close-button" src="./assets/icons/close.png" alt="close-icon">
-        </div>
+    `;
 
-        <div class="create-field">
-            <div class="text-create-field">Create Task</div>
-            <img class="check-button" src="./assets/icons/check.png" alt="check-button">
-        </div>
-    </div>
-    </div>
-    </div>
-    </div>
-    `
-    // Animation erneut triggern (falls mehrfach geöffnet)
-    const contentTodo = overlayToDoContainer.querySelector('.overlay-todo-contentTodo');
-    contentTodo.style.animation = 'none';
-    void contentTodo.offsetWidth; // Reflow erzwingen
-    contentTodo.style.animation = '';
-    contentTodo.classList.add('slide-in');
+    // Animation anwenden
+    const contentTodoElements = overlayToDoContainer.getElementsByClassName('overlay-todo-contentTodo');
+    const contentTodo = contentTodoElements[contentTodoElements.length - 1];
+    if (contentTodo) {
+        contentTodo.style.animation = 'none';
+        void contentTodo.offsetWidth;
+        contentTodo.style.animation = '';
+        contentTodo.classList.add('slide-in');
+    }
 
+    // Klick außerhalb des Inhalts => Overlay schließen
+    overlayToDoContainer.onclick = function () {
+        closeOverlayToDo();
+    };
 }
 
-function closeOverlayToDo(event) {
-    document.getElementById('overlay-todo').classList.remove('overlay-visible');
+function closeOverlayToDo() {
+    const overlay = document.getElementById('overlay-todo');
+    if (overlay) {
+        overlay.classList.remove('overlay-visible');
+    }
 }
+
+
 
 function openOverlayInProgress() {
     const overlayInProgress = document.getElementById('overlay-in-progress');
@@ -245,7 +242,6 @@ function openOverlayInProgress() {
     let overlayProgress = document.getElementById('content-add-task-overlay-in-progress');
     overlayProgress.innerHTML += `
     <div class="container-both-sides">
-    
             <div class="inputfield-box-left-side">
                 <div class="text-title">
                     <div class="text-title-mark">
@@ -484,20 +480,20 @@ function closeOverlayFeedback(event) {
 }
 
 
-    function selectContacts() {
-        const dropdown = document.getElementById('contacts-dropdown');
-        dropdown.classList.toggle('hidden');
-    }
+function selectContacts() {
+    const dropdown = document.getElementById('contacts-dropdown');
+    dropdown.classList.toggle('hidden');
+}
 
 
-    function toggleRed(button) {
-        button.classList.toggle('active');
-    }
+function toggleRed(button) {
+    button.classList.toggle('active');
+}
 
-    function toggleOrange(button) {
-        button.classList.toggle('active');
-    }
+function toggleOrange(button) {
+    button.classList.toggle('active');
+}
 
-    function toggleGreen(button) {
-        button.classList.toggle('active');
-    }
+function toggleGreen(button) {
+    button.classList.toggle('active');
+}
