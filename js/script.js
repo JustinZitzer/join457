6273cee821b5a0d9ce54bd4c1f173c0fe9c1f60b
Marvin Mutwil <marvinmutwil@hotmail.de>
2025-07-07 00:47:27 +0200

    Optimize font loading and trigger resize on navigation
    Added preconnect links to Google Fonts in all relevant HTML files to improve font loading performance. Updated navigation functions in script.js to dispatch a resize event after changing pages, ensuring responsive layout adjustments.

--- js/script.js
+++ js/script.js
@@ -5,16 +5,20 @@ function toggleUserProfileOverlay() {
 
 function goToSummaryHtml() {
     window.location.href = 'summary.html';
+    window.dispatchEvent(new Event('resize'));
 }
 
 function goToAddTaskHtml() {
     window.location.href = 'add_task.html';
+    window.dispatchEvent(new Event('resize'));
 }
 
 function goToBoardHtml() {
     window.location.href = 'board.html';
+    window.dispatchEvent(new Event('resize'));
 }
 
 function goToContactsHtml() {
     window.location.href = 'contacts.html';
+    window.dispatchEvent(new Event('resize'));
 }
