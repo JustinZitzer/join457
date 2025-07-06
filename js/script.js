523c29af43443559f917cd3e8b8beb0daf25f1f2
Marvin Mutwil <marvinmutwil@hotmail.de>
2025-07-07 00:11:21 +0200

    Changed the links to an onclick function
    Changed it, so we can handle better with our other js code.

--- js/script.js
+++ js/script.js
@@ -2,3 +2,19 @@ function toggleUserProfileOverlay() {
     const userProfileMenu = document.getElementById('user_profile_menu');
     userProfileMenu.classList.toggle('hide')
 }
+
+function goToSummaryHtml() {
+    window.location.href = 'summary.html';
+}
+
+function goToAddTaskHtml() {
+    window.location.href = 'add_task.html';
+}
+
+function goToBoardHtml() {
+    window.location.href = 'board.html';
+}
+
+function goToContactsHtml() {
+    window.location.href = 'contacts.html';
+}
