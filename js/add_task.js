e40c3d2435d24fa000fa5ed4a70c311dc77db4ab
Marvin Mutwil <marvinmutwil@hotmail.de>
2025-08-04 02:39:06 +0200

    Subtask input UI and logic changed
    Updated the subtask input field in add_task.html to use a new layout with plus, check, and cross icons for improved user interaction. New function, logic to show/hide icons and clear input, streamlining the subtask addition workflow.

--- js/add_task.js
+++ js/add_task.js
@@ -289,31 +289,22 @@ function addSubtaskInContainer() {
   return { subtask, subtaskSavedCounter };
 }
 
-function addNewStyleToSubtaskInputField() {
-  const changedSubtaskInputField = document.getElementById("changed-subtask-inputfield-id");
-  if (!changedSubtaskInputField) {
-  subtaskInputFieldContainer.innerHTML = `
-      <div id="changed-subtask-inputfield-id">
-        <span>Subtasks</span>
-        <div class="subtask-input-icon-flexbox">
-          <input onclick="addNewStyleToSubtaskInputField()" type="text" id="inputfield-subtask-assign" placeholder="Add new subtask" class="inputfield-subtask-assign">
-          <span class="input-icon-container">
-                <svg onclick="clearSubtaskInputField()" class="subtask-icon-cross" width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M5 5L15 15M15 5L5 15" stroke="#2A3647" stroke-width="2" stroke-linecap="round"/>
-                </svg>
-                <div class="subtask-icon-separator"></div>
-                <svg class="subtask-icon-check" width="16" height="16" viewBox="0 0 38 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M4.02832 15.0001L15.2571 26.0662L33.9717 3.93408" stroke="#2A3647" stroke-width="4" stroke-linecap="round" stroke-linejoin="round"/>
-                </svg>
-            </span>
-        </div>
-      </div>
-    `;
-  }
+function hidePlusIconShowCheckAndCrossIcon() {
+  const plusIcon = document.getElementById("add-icon-container");
+  const checkIcon = document.getElementById("subtask-icon-cross");
+  const separatorIcon = document.getElementById("subtask-icon-separator");
+  const checkIconCheck = document.getElementById("subtask-icon-check");
+  plusIcon.classList.add("display-none");
+  checkIcon.classList.remove("display-none");
+  separatorIcon.classList.remove("display-none");
+  checkIconCheck.classList.remove("display-none");
 }
 
 function clearSubtaskInputField() {
-  taskSubtask.value = "";
+  const taskSubtask = document.getElementById("inputfield-subtask-assign");
+    if (taskSubtask) {
+        taskSubtask.value = "";
+    }
 }
 
 function editSavedSubtask(subtaskSavedCounter, subtask) {
