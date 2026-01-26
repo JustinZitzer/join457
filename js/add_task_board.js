7fbc1113ab1f708ff1556d03f0543e142ab4de32
Marvin Mutwil <marvinmutwil@hotmail.de>
2026-01-26 01:30:33 +0100

    Check icon has now an right size and  message an *.

--- js/add_task_board.js
+++ js/add_task_board.js
@@ -619,7 +619,7 @@ function showErrorForNewTaskDueDate() {
     errorMessage.classList.add("display-none");
     dueDateInput.classList.add("border-red-add-task");
     dueDateErrorMessage.classList.remove("display-none");
-    dueDateErrorMessage.textContent = "Please pick a date TT/MM/YYYY.";
+    dueDateErrorMessage.textContent = "*Please pick a date TT/MM/YYYY.";
   }
 }
 
