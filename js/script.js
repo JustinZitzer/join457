6200fd23b3ac84b04629c26504ea05bb1f968b58
Marvin Mutwil <marvinmutwil@hotmail.de>
2026-04-10 20:42:49 +0200

    Js doc for script.js

--- js/script.js
+++ js/script.js
@@ -1,23 +1,28 @@
+/** Toggles the visibility of the user profile overlay menu. */
 function toggleUserProfileOverlay() {
     const userProfileMenu = document.getElementById('user_profile_menu');
     userProfileMenu.classList.toggle('hide')
 }
 
+/** Navigates to the summary page and triggers a resize event. */
 function goToSummaryHtml() {
     window.location.href = 'summary.html';
     window.dispatchEvent(new Event('resize'));
 }
 
+/** Navigates to the add task page and triggers a resize event. */
 function goToAddTaskHtml() {
     window.location.href = 'add_task.html';
     window.dispatchEvent(new Event('resize'));
 }
 
+/** Navigates to the board page and triggers a resize event. */
 function goToBoardHtml() {
     window.location.href = 'board.html';
     window.dispatchEvent(new Event('resize'));
 }
 
+/** Navigates to the contacts page and triggers a resize event. */
 function goToContactsHtml() {
     window.location.href = 'contacts.html';
     window.dispatchEvent(new Event('resize'));
